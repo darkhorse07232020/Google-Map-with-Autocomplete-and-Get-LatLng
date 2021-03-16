93100c9314e2dcf0b2dcfa1112883ab8494887f6
Darkhorse <darkhorse0723@outlook.com>
2021-03-16 21:25:57 +0300

    Create API

---
+++ Readme.md
@@ -0,0 +1,5 @@
+This is the web page and API to get latitude and longitude from address.
+User can get geometry info with 2 method.
+#First
+If you enter this url: https://your_url/, you can get the Search Form.
+<img width="100%" src="screenshot_1.png" align="center" alt="GitHub Readme Stats" />
--- index.php
+++ index.php
@@ -8,8 +8,43 @@
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="assets/index.js"></script>
   </head>
+  
   <body>
     <div id="address" class="p-3">
+
+<?php
+if (isset($_REQUEST['address'])) {
+     $address = urlencode($_REQUEST['address']);
+     $url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='.$address.'&inputtype=textquery&fields=formatted_address,geometry&key=AIzaSyDH1-nya9mZdJpxk2UMZvFh6eXTYb3vQFM';
+     $response = file_get_contents($url);
+     $res_json = json_decode($response, true);
+     if($res_json['status']=='OK'){
+ 
+          // get the important data
+          $lat = isset($res_json['candidates'][0]['geometry']['location']['lat']) ? $res_json['candidates'][0]['geometry']['location']['lat'] : "";
+          $lng = isset($res_json['candidates'][0]['geometry']['location']['lng']) ? $res_json['candidates'][0]['geometry']['location']['lng'] : "";
+          $full_address = isset($res_json['candidates'][0]['formatted_address']) ? $res_json['candidates'][0]['formatted_address'] : "";
+           
+      }
+?>
+      <div class="py-2">
+          <input class="field" id="route" value="<?php echo $full_address ?>"/>
+      </div>
+      <div class="row py-2">
+        <div class="col-3 text-right">Latitude</div>
+        <div class="col-9">
+          <input class="field" id="lat" value="<?php echo $lat ?>"/>
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col-3 text-right">Longitude</div>
+        <div class="col-9">
+          <input class="field" id="lng" value="<?php echo $lng ?>"/>
+        </div>
+      </div>
+<?php
+} else {
+?>
       <div class="py-2">
         <input
           id="autocomplete"
@@ -61,11 +96,13 @@
           <input class="field" id="lng"/>
         </div>
       </div>
-    </table>
-
+      <?php
+}
+?>
+    </div>
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoY4Yn60USF1fDNIm65QVpRBowNeBBgbA&callback=initAutocomplete&libraries=places&v=weekly"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDH1-nya9mZdJpxk2UMZvFh6eXTYb3vQFM&callback=initAutocomplete&libraries=places&v=weekly"
       async
     ></script>
   </body>
